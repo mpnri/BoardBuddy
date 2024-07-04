fe70611d782c40cc0ef095793059d53a19c8ff65
mpnri <mpnri2831@gmail.com>
2024-07-05 02:53:07 +0330

    fix(frontend): Disabled serializable check

--- Frontend/src/store.ts
+++ Frontend/src/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { AuthReducers } from "./auth/auth.slice";
+import { UsersReducers } from "./users/users.slice";
+import { enableMapSet } from "immer";
+enableMapSet();
 
 export const store = configureStore({
   reducer: {
+    users: UsersReducers,
     auth: AuthReducers,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
