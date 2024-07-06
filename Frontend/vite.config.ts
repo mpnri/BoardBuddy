bc30132f226dcae88b1a9b872aab3253cdbd1644
mpnri <mpnri2831@gmail.com>
2024-07-06 13:34:08 +0330

    feat(fronted): Adds media scss

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,4 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "~", replacement: resolve(__dirname, "./src") }],
+  },
 });
